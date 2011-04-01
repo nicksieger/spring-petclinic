095ef64911d4a2dce2e9398849996817ca6c3f00
Nick Sieger <nick@nicksieger.com>
2011-04-01 11:22:11 -0500

    Add vets feature that tests xml and json output

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -17,6 +17,12 @@ module NavigationHelpers
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
 
+    when /the vets page/
+      '/vets'
+
+    when %r{^/[^ ]+}
+      page_name
+
     else
       begin
         page_name =~ /the (.*) page/
