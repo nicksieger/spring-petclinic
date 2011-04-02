84648ebb09043e00cdbcea4bfe9fbc55f6df22d2
Nick Sieger <nick@nicksieger.com>
2011-04-01 20:17:54 -0500

    Modify #form_for application-wide to change id attribute convention
    This allows Cucumber/capybara/celerity tests to work unmodified
    between the Rails and Spring apps

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,35 @@ module ApplicationHelper
       content
     end
   end
+
+  # This little wrapper around #form_for is so that we can make
+  # consistent form element IDs between the Spring and the Rails
+  # version of the app so that Cucumber/Capybara/Celerity tests can
+  # remain the same.
+  class FormIdWrapper
+    def initialize(form)
+      @form = form
+    end
+
+    def respond_to?(*args)
+      @form.respond_to?(*args)
+    end
+
+    def method_missing(meth,*args,&block)
+      if Symbol === args.first
+        options = args.extract_options!
+        options[:id] ||= args.first
+        args << options
+        @form.send(meth, *args, &block)
+      else
+        @form.send(meth, *args, &block)
+      end
+    end
+  end
+
+  def form_for(*args,&block)
+    super(*args, &Proc.new do |f|
+            block.call FormIdWrapper.new(f)
+          end)
+  end
 end
